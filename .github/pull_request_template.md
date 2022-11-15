204fc36b62d4d1917cffb3e0157e2178d8de3cd5
Alex Standiford <a@alexstandiford.com>
2022-11-15 06:53:53 -0600

    Initial commit.

---
+++ .github/pull_request_template.md
@@ -0,0 +1,19 @@
+## Summary
+
+<!-- Add a 2-3 sentence description of that this PR does -->
+
+## Before Marking this PR as Ready to Review, I have verified that:
+
+- [ ] This PR fulfills the acceptance criteria detailed in the issue.
+- [ ] This PR matches the coding standards.
+- [ ] All validations have passed.
+- [ ] I have assigned this PR to myself
+- [ ] I have linked this PR to the issue it is intended to close.
+
+## Before Merge, the Reviewer has verified that:
+
+- [ ] This PR fulfills the acceptance criteria detailed in the issue.
+- [ ] I have reviewed and understand what was written.
+- [ ] The document is written and communicated clearly.
+- [ ] This PR is free of typos, and other grammatical errors.
+- [ ] All checks have passed.
---
+++ docusaurus.config.js
@@ -0,0 +1,95 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require( 'prism-react-renderer/themes/github' )
+const darkCodeTheme = require( 'prism-react-renderer/themes/dracula' )
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Adiungo Core Documentation',
+    tagline: 'Adiungo (a AOON go) - To gather, collect, or add to a collection.',
+    url: 'https://docs.adiungo.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'adiungo', // Usually your GitHub org/user name.
+    projectName: 'core', // Usually your repo name.
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            ( {
+                blog: false,
+                docs: {
+                    sidebarPath: require.resolve( './sidebars.js' ),
+                    routeBasePath: '/'
+                },
+            } ),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ( {
+            navbar: {
+                title: 'Adiungo',
+                // logo: {
+                //   alt: 'My Site Logo',
+                //   src: 'img/logo.svg',
+                // },
+                items: [
+                    {
+                        href: 'https://github.com/adiungo/core',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Ask a Question (GitHub Discussions)',
+                                href: 'https://github.com/adiungo/core/discussions',
+                            },
+                            {
+                                label: 'Element (Chat)',
+                                href: 'https://matrix.to/#/#project-adiungo:matrix.org',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Alex Standiford.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        } ),
+}
+
+module.exports = config
